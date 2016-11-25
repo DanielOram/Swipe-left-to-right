b8dbed036e29738fef1c343f33bf127f4cf8ea42
Daniel Oram <danieloramnz@gmail.com>
2016-11-25 13:49:06 +1300

    enable swiping action from R to L with default delete action

--- Swipe-left-to-right/SwipeyTableViewController.swift
+++ Swipe-left-to-right/SwipeyTableViewController.swift
@@ -58,7 +58,7 @@ class SwipeyTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -68,7 +68,7 @@ class SwipeyTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
